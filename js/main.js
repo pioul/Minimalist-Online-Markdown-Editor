5f024b2ce3b60202b34aa58eb17401c907497af8
Philippe Masset <pioul06+github@gmail.com>
2013-11-28 22:59:33 +0100

    Made code more legible
    Updated comments, indentation and spacing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(document).on("ready", function(){
+$(document).on("ready", function() {
 	
 	editor = {
 		
-		// variables
+		// Editor variables
 		fitHeightElements: $(".full-height"),
 		wrappersMargin: $("#left-column > .wrapper:first").outerHeight(true) - $("#left-column > .wrapper:first").height(),
 		markdownConverter: new Showdown.converter(),
@@ -19,8 +19,8 @@ $(document).on("ready", function(){
 		isFullscreen: false,
 		supportsLocalStorage: ("localStorage" in window && window.localStorage !== null),
 		
-		// functions
-		init: function(){
+		// Initiate editor
+		init: function() {
 			this.onloadEffect(0);
 			this.bind();
 			this.switchToTarget("preview");
@@ -29,72 +29,85 @@ $(document).on("ready", function(){
 			this.convertMarkdown();
 			this.onloadEffect(1);
 		},
-		bind: function(){
-			$(window).on("resize", function(){
+
+		// Handle events on several DOM elements
+		bind: function() {
+			$(window).on("resize", function() {
 				editor.fitHeight();
 			});
-			this.markdownSource.on("keyup change", function(){
-				editor.markdownSource.trigger("change.editor");
-			});
-			this.markdownSource.on("cut paste drop", function(){
-				setTimeout(function(){
+			this.markdownSource.on({
+				"keyup change": function() {
 					editor.markdownSource.trigger("change.editor");
-				}, 0);
+				},
+				"cut paste drop": function() {
+					setTimeout(function() {
+						editor.markdownSource.trigger("change.editor");
+					}, 0);
+				},
+				"change.editor": function() {
+					editor.saveMarkdown();
+					editor.convertMarkdown();
+				}
 			});
-			this.markdownSource.on("change.editor", function(){
-				editor.saveMarkdown();
-				editor.convertMarkdown();
-			});
-			this.markdownTargetsTriggers.on("click", function(e){
+			this.markdownTargetsTriggers.on("click", function(e) {
 				e.preventDefault();
 				editor.switchToTarget($(this).data("switchto"));
 			});
-			this.topPanelsTriggers.on("click", function(e){
+			this.topPanelsTriggers.on("click", function(e) {
 				e.preventDefault();
 				editor.toggleTopPanel($(this).data("toppanel"));
 			});
-			this.topPanels.children(".close").on("click", function(e){
+			this.topPanels.children(".close").on("click", function(e) {
 				e.preventDefault();
 				editor.closeTopPanel();
 			});
-			this.quickReferencePreText.on("click", function(){
+			this.quickReferencePreText.on("click", function() {
 				editor.addToMarkdownSource($(this).text());
 			});
-			this.featuresTriggers.on("click", function(e){
+			this.featuresTriggers.on("click", function(e) {
 				e.preventDefault();
 				var t = $(this);
 				editor.toggleFeature(t.data("feature"), t.data());
 			});
 		},
-		fitHeight: function(){
+
+		// Resize some elements to make the editor fit inside the window
+		fitHeight: function() {
 			var newHeight = $(window).height() - this.wrappersMargin;
-			this.fitHeightElements.each(function(){
-				if($(this).closest("#left-column").length){
+			this.fitHeightElements.each(function() {
+				var t = $(this);
+				if (t.closest("#left-column").length) {
 					var thisNewHeight = newHeight - $("#top_panels_container").outerHeight();
 				} else {
 					var thisNewHeight = newHeight;
 				}
-				$(this).css({ height: thisNewHeight +"px" });
+				t.css({ height: thisNewHeight +"px" });
 			});
 		},
-		saveMarkdown: function(){
-			if(!this.supportsLocalStorage) return false;
+
+		// Save Markdown in localStorage
+		saveMarkdown: function() {
+			if (!this.supportsLocalStorage) return false;
 			var markdown = this.markdownSource.val();
-			// even if localStorage is supported, using it can still throw an exception if disabled or the quota is exceeded
+			// Even if localStorage is supported, using it can still throw an exception if disabled or the quota is exceeded
 			try {
 				localStorage.setItem("markdown", markdown);
-			} catch(e){}
+			} catch (e) {}
 		},
-		loadMarkdown: function(){
-			if(!this.supportsLocalStorage) return false;
+
+		// Load Markdown from localStorage
+		loadMarkdown: function() {
+			if (!this.supportsLocalStorage) return false;
 			var markdown;
-			// even if localStorage is supported, using it can still throw an exception if disabled
+			// Even if localStorage is supported, using it can still throw an exception if disabled
 			try {
 				markdown = localStorage.getItem("markdown");
-			} catch(e){}
-			if(markdown) this.markdownSource.val(markdown);
+			} catch (e) {}
+			if (markdown) this.markdownSource.val(markdown);
 		},
-		convertMarkdown: function(){
+
+		// Convert Markdown to HTML using showdown.js
+		convertMarkdown: function() {
 			var markdown = this.markdownSource.val(),
 				html = this.markdownConverter.makeHtml(markdown);
 			$("#html").val(html);
@@ -102,30 +115,36 @@ $(document).on("ready", function(){
 				.html(html)
 				.trigger("updated.editor");
 		},
-		addToMarkdownSource: function(markdown){
+
+		// Programmatically add Markdown text to the textarea
+		addToMarkdownSource: function(markdown) {
 			var markdownSourceValue = this.markdownSource.val();
-			if(markdownSourceValue != "") markdownSourceValue += "\n\n";
+			if (markdownSourceValue != "") markdownSourceValue += "\n\n";
 			this.markdownSource.val(markdownSourceValue + markdown);
 			this.markdownSource.trigger("change.editor");
 		},
-		switchToTarget: function(which){
+
+		// Switch between editor panels
+		switchToTarget: function(which) {
 			var target = $("#"+ which),
 				targetTrigger = this.markdownTargetsTriggers.filter("[data-switchto="+ which +"]");
-			if(!this.isFullscreen || which != "markdown") this.markdownTargets.not(target).hide();
+			if (!this.isFullscreen || which != "markdown") this.markdownTargets.not(target).hide();
 			target.show();
 			this.markdownTargetsTriggers.not(targetTrigger).removeClass("active");
 			targetTrigger.addClass("active");
-			if(which != "markdown") this.featuresTriggers.filter("[data-feature=fullscreen][data-tofocus]").last().data("tofocus", which);
-			if(this.isFullscreen){
+			if (which != "markdown") this.featuresTriggers.filter("[data-feature=fullscreen][data-tofocus]").last().data("tofocus", which);
+			if (this.isFullscreen) {
 				var columnToShow = (which == "markdown")? this.markdownSource.closest(this.columns) : this.markdownPreview.closest(this.columns);
 				columnToShow.show();
 				this.columns.not(columnToShow).hide();
 			}
-			if(this.isAutoScrolling && which == "preview"){
-				this.markdownPreview.trigger("updated.editor"); // auto-scroll on switch since it wasn't earlier due to the preview being hidden
+			if (this.isAutoScrolling && which == "preview") {
+				this.markdownPreview.trigger("updated.editor"); // Auto-scroll on switch since it wasn't possible earlier due to the preview being hidden
 			}
 		},
-		toggleTopPanel: function(which){
+
+		// Toggle top panels visibility
+		toggleTopPanel: function(which) {
 			var panel = $("#"+ which),
 				panelTrigger = this.topPanelsTriggers.filter("[data-toppanel="+ which +"]");
 			this.topPanels.not(panel).hide();
@@ -134,14 +153,17 @@ $(document).on("ready", function(){
 			panelTrigger.toggleClass("active");
 			this.fitHeight();
 		},
-		closeTopPanel: function(){
+
+		closeTopPanel: function() {
 			this.topPanels.hide();
 			this.topPanelsTriggers.removeClass("active");
 			this.fitHeight();
 		},
-		toggleFeature: function(which, featureData){
+
+		// Toggle editor feature
+		toggleFeature: function(which, featureData) {
 			var featureTrigger = this.featuresTriggers.filter("[data-feature="+ which +"]");
-			switch(which){
+			switch (which) {
 				case "auto-scroll":
 					this.toggleAutoScroll();
 					break;
@@ -151,12 +173,13 @@ $(document).on("ready", function(){
 			}
 			featureTrigger.toggleClass("active");
 		},
-		toggleAutoScroll: function(){
-			if(!this.isAutoScrolling){
+
+		toggleAutoScroll: function() {
+			if (!this.isAutoScrolling) {
 				this.markdownPreview
-					.on("updated.editor", function(){
+					.on("updated.editor", function() {
 						var markdownPreview = this;
-						setTimeout(function(){
+						setTimeout(function() {
 							markdownPreview.scrollTop = markdownPreview.scrollHeight;
 						}, 0);
 					})
@@ -166,34 +189,39 @@ $(document).on("ready", function(){
 			}
 			this.isAutoScrolling = !this.isAutoScrolling;
 		},
-		toggleFullscreen: function(featureData){
+
+		toggleFullscreen: function(featureData) {
 			var toFocus = featureData.tofocus;
 			this.isFullscreen = !this.isFullscreen;
 			$(document.body).toggleClass("fullscreen");
-			if(toFocus) this.switchToTarget(toFocus);
-			if(!this.isFullscreen){
-				this.columns.show(); // make sure all columns are visible when exiting fullscreen
+			if (toFocus) this.switchToTarget(toFocus);
+			// Exit fullscreen
+			if (!this.isFullscreen) {
+				this.columns.show(); // Make sure all columns are visible when exiting fullscreen
 				var activeMarkdownTargetsTriggersSwichtoValue = this.markdownTargetsTriggers.filter(".active").first().data("switchto");
-				// force one of the right panel's elements to be active if not already when exiting fullscreen
-				if(activeMarkdownTargetsTriggersSwichtoValue == "markdown"){
+				// Force one of the right panel's elements to be active if not already when exiting fullscreen
+				if (activeMarkdownTargetsTriggersSwichtoValue == "markdown") {
 					this.switchToTarget("preview");
 				}
-				// auto-scroll when exiting fullscreen and "preview" is already active since it changes width
-				if(this.isAutoScrolling && activeMarkdownTargetsTriggersSwichtoValue == "preview"){
+				// Auto-scroll when exiting fullscreen and "preview" is already active since it changes width
+				if (this.isAutoScrolling && activeMarkdownTargetsTriggersSwichtoValue == "preview") {
 					this.markdownPreview.trigger("updated.editor");
 				}
 				$(document).off(".fullscreen");
+			// Enter fullscreen
 			} else {
 				this.closeTopPanel();
-				// exit fullscreen when the escape key is pressed
-				$(document).on("keyup.fullscreen", function(e){
-					if(e.keyCode == 27) editor.featuresTriggers.filter("[data-feature=fullscreen]").last().trigger("click");
+				// Exit fullscreen when the escape key is pressed
+				$(document).on("keyup.fullscreen", function(e) {
+					if (e.keyCode == 27) editor.featuresTriggers.filter("[data-feature=fullscreen]").last().trigger("click");
 				});
 			}
 		},
-		onloadEffect: function(step){
+
+		// Subtle fade-in effect
+		onloadEffect: function(step) {
 			var theBody = $(document.body);
-			switch(step){
+			switch (step) {
 				case 0:
 					theBody.fadeTo(0, 0);
 					break;
