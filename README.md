940aef5a1dcc3932fef9da27aa44e35f3ea30dc1
Philippe Masset <pioul06+github@gmail.com>
2015-01-31 15:33:05 +0100

    Add build process

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Minimalist Online Markdown Editor
 
-This is the source for the **simplest** and **slickest** online Markdown editor, available online here: http://markdown.pioul.fr  
+This is the source for the **simplest** and **slickest** online Markdown editor, available online here: <http://markdown.pioul.fr>  
 Just write Markdown and see what it looks like as you type. And convert it to HTML in one click.
 
 ## The Minimalist Markdown Editor project
@@ -12,16 +12,24 @@ The Minimalist Markdown Editor project is available both online as a web app, an
 
 ## Contributing
 
+### Building
+
+1. Ensure that [Node.js](http://nodejs.org/) is installed, and open a terminal in the project's root directory.
+2. Run `npm install` to install the project's developement dependencies.
+3. Run `npm run build` to build the web app. The builds will be placed in the `dist/` directory.
+
+Note: Building should only be necessary if you think about contributing. If you want to run one of the apps, hit one of the links above.
+
 ### Git workflow
 
 The two project branches share a decent amount of code. This common source code sits in `app-shared/` in each of these repos. As you can see from the commit history, changes to files in- and outside of `app-shared/` are committed separately to help with cherry-picking the common source changes from the other repo afterward.
 
-E.g. You've made changes to `css/main.css` and `app-shared/css/main.css`. Since changes have been made to files in- and outside of `app-shared/`, instead of committing all changes at once, make two commits:
+E.g. You've made changes to `src/css/main.css` and `src/app-shared/css/main.css`. Since changes have been made to files in- and outside of `app-shared/`, instead of committing all changes at once, make two commits:
 
-1. The first one will be called "[app-shared] commit_message", and will commit changes to `app-shared/css/main.css`.
-2. The second one will be called "commit_message", and will commit changes to `css/main.css`.
+1. The first one will be called "[app-shared] commit_message", and will commit changes to `src/app-shared/css/main.css`.
+2. The second one will be called "commit_message", and will commit changes to `src/css/main.css`.
 
-If changes had only been made to `css/main.css`, then there would've been no need for the first commit. And if changes had only been made to `app-shared/css/main.css`, there would've been no need for the second commit.
+If changes had only been made to `src/css/main.css`, then there would've been no need for the first commit. And if changes had only been made to `src/app-shared/css/main.css`, there would've been no need for the second commit.
 
 That's really all there is to know about this project's Git workflow, so fork away!
 
---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+var gulp = require("gulp"),
+	uglify = require("uglify-js"),
+	CleanCss = require("clean-css"),
+	del = require("del"),
+	through = require("through2"),
+	vinylMap = require("vinyl-map"), // through2 abstraction for accessing file contents only
+	path = require("path"),
+
+	paths = {
+		base: {
+			root: "",
+			src: "src/",
+			dest: "dist/"
+		},
+		js: {
+			srcGlob: [
+				"src/app-shared/js/libs/**",
+				"src/app-shared/js/**",
+				"src/js/**",
+			],
+			relDest: "js/all.js",
+			relRevDest: null
+		},
+		css: {
+			srcGlob: "src/app-shared/css/**",
+			srcDir: "src/app-shared/css/",
+			relDest: "css/all.css",
+			relRevDest: null
+		},
+		html: {
+			src: "src/index.html"
+		},
+
+		// Negate a glob string or array
+		negateGlob: function(glob) {
+			var negate = function(str) {
+				// If globstar alone in last path portion, also exclude the empty directory itself
+				if (str.slice(-3) == "/**") str = str.slice(0, -3) +"{,/**}";
+
+				return "!"+ str;
+			};
+
+			if (glob instanceof Array) return glob.map(negate);
+				else return negate(glob);
+		}
+	},
+
+	gulpPlugins = {
+		concat: require("gulp-concat"),
+		size: require("gulp-size"),
+		revision: require("gulp-rev"),
+		htmlReplace: require("gulp-html-replace"),
+
+		saveRevFileName: function(assetType) {
+			var getRevFileName = function(file) {
+					var sep = "/",
+						fileBase = file.base.split(path.sep).join(sep), // Replace all without having to escape special regex chars
+						filePath = file.path.split(path.sep).join(sep);
+
+					filePath = filePath.replace(fileBase, "");
+					if (filePath[0] == sep) filePath = filePath.slice(1); // Make sure this is a relative path
+
+					return filePath;
+				},
+
+				write = function(file, enc, cb) {
+					paths[assetType].relRevDest = getRevFileName(file);
+					cb(null, file);
+				};
+
+			return through({ objectMode: true }, write);
+		}
+	};
+
+// Empty paths.base.dest
+gulp.task("clean", function(c) {
+	del(paths.base.dest +"**", c);
+});
+
+// JS files: concat + minify + cache-bust
+gulp.task("build-js", ["clean"], function() {
+	var displayOriginalSize = gulpPlugins.size({
+			title: "Original JS size"
+		}),
+
+		displayFinalSize = gulpPlugins.size({
+			title: "Minified + gzipped JS size",
+			gzip: true
+		}),
+
+		minifyJs = vinylMap(function(contents) {
+			return uglify.minify(contents.toString(), {
+				fromString: true
+			}).code;
+		});
+
+	return gulp.src(paths.js.srcGlob, { base: paths.base.src })
+		.pipe(gulpPlugins.concat(paths.js.relDest))
+		.pipe(displayOriginalSize)
+		.pipe(minifyJs)
+		.pipe(displayFinalSize)
+		.pipe(gulpPlugins.revision())
+		.pipe(gulpPlugins.saveRevFileName("js"))
+		.pipe(gulp.dest(paths.base.dest));
+});
+
+// CSS files: concat + minify + cache-bust
+gulp.task("build-css", ["clean"], function() {
+	var displayOriginalSize = gulpPlugins.size({
+			title: "Original CSS size"
+		}),
+
+		displayFinalSize = gulpPlugins.size({
+			title: "Minified + gzipped CSS size",
+			gzip: true
+		}),
+
+		minifyCss = vinylMap(function (contents) {
+			return new CleanCss({
+				relativeTo: paths.css.srcDir, // Path to resolve relative URLs
+				target: paths.base.src + paths.css.relDest // Path to a folder or an output file to which rebase all URLs; has to use the same paths.base as relativeTo to avoid confusing CleanCss
+			}).minify(contents.toString()).styles;
+		});
+
+	return gulp.src(paths.css.srcGlob, { base: paths.base.src })
+		.pipe(displayOriginalSize)
+		.pipe(minifyCss)
+		.pipe(gulpPlugins.concat(paths.css.relDest))
+		.pipe(displayFinalSize)
+		.pipe(gulpPlugins.revision())
+		.pipe(gulpPlugins.saveRevFileName("css"))
+		.pipe(gulp.dest(paths.base.dest));
+});
+
+// Revise assets' references. Currently only bothers to do that in index.html, since JS and CSS are the only
+// revised assets; will have to extend to CSS files once fonts and images are also revised.
+gulp.task("revise-assets-refs", ["build-js", "build-css"], function() {
+	return gulp.src(paths.html.src, { base: paths.base.src })
+		.pipe(gulpPlugins.htmlReplace({
+			js: paths.js.relRevDest,
+			css: paths.css.relRevDest
+		}))
+		.pipe(gulp.dest(paths.base.dest));
+});
+
+// Simply copy the rest of the files from src/ to dist/
+gulp.task("copy", ["clean"], function() {
+	var src = [].concat(
+		"src/**",
+		paths.negateGlob(paths.js.srcGlob),
+		paths.negateGlob(paths.css.srcGlob),
+		paths.negateGlob(paths.html.src)
+	);
+
+	return gulp.src(src, { base: paths.base.src })
+		.pipe(gulp.dest(paths.base.dest));
+});
+
+gulp.task("default", ["build-js", "build-css", "revise-assets-refs", "copy"]);
